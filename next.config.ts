337c45bbd39fc54430196cd5dda7085656f048d3
Saurav Maherchandani <saurav.maherchandani@technostacks.com>
2024-11-21 19:38:28 +0530

    second commit

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['next-project-image-upload-testing.s3.us-east-1.amazonaws.com'],
+    unoptimized: true // Add this for static export
   },
+  output: 'export' // Explicitly set export mode
 };
 
 export default nextConfig;
