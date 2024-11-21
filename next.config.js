7b5e685784b0a8f2bbae901ad9e1d221e35bf427
Saurav Maherchandani <saurav.maherchandani@technostacks.com>
2024-11-21 20:44:19 +0530

    fifth commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   images: {
     domains: ['next-project-image-upload-testing.s3.us-east-1.amazonaws.com'],
     unoptimized: true // Add this for static export
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
